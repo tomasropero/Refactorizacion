3578f6d575b9597aab1ee81f1c0789c9e80c6883
Alumnos DAW <Alumnos DAW@PC-CZC44374TN>
2025-04-03 18:35:42 +0200

    Agregada la clase Calculadora

---
+++ src/Prueba/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prueba;
+
+/**
+ *
+ * @author Alumnos DAW
+ */
+public class Calculadora {
+    public int suma(int a, int b){
+        return a+b;
+    }
+}
